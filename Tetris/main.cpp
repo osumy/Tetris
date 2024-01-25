c1decbc91b0c67b38247ff9e9f040aa23ca3ae4c
AmirAli Araghi <amirali40223@gmail.com>
2024-01-25 21:47:39 +0330

    fix

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <Windows.h>
-#include <thread>
 #include "menu.h"
 
 #pragma comment(lib, "winmm.lib")
@@ -9,6 +8,7 @@ using namespace std;
 
 int main()
 {
+	PlaySound(TEXT("main.wav"), NULL, SND_ASYNC | SND_FILENAME);
 	mainMenu();
 	return 0;
 }
